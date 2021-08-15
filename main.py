7ce1c479a8bd5169c77a9a3a95c531213784b261
NIZAR BAMIDA <bamidanizar@gmx.com>
2021-08-15 05:54:00 +0100

    aded requirements

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-DRIVER_PATH = '/path/to/browser'
 
+DRIVER_PATH = 'path/to/driver'
 
 
 # if you want to use chromedriver
@@ -14,3 +15,16 @@ driver = webdriver.Chrome(executable_path=DRIVER_PATH)
 
 
 driver.get('https://google.com')
+
+try:
+    element = WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, "gLFyf")))
+finally:
+    driver.quit()
+    
+    
+    
+    
+    
+    
+    
